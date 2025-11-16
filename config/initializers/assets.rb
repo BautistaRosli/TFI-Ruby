b7e2dd8214e5a91035156c7e20c77c4703833060
Arkimss <acostasalim54@gmail.com>
2025-11-16 18:12:12 +0000

    agrego carpeta para poner js en los assets, ventana de confirmación de eliminación de ventas y estilos de paginación para todos los listados de la aplicación

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,3 +4,6 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
+
+# Precompile additional assets
+Rails.application.config.assets.precompile += %w( admin_layout.css sales_index.css users_index.css admin_index.css custom_modal.css custom_modal.js )
