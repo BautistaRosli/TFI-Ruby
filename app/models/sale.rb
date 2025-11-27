9c3a126f54d9c0e123b9dda0297783abd2c30968
Arkimss <acostasalim54@gmail.com>
2025-11-27 02:16:23 +0000

    Agrego callback para devolver el stock de los discos cuando se elimina una venta

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,7 +1,13 @@
 class Sale < ApplicationRecord
   #Relacion con items de venta, 
-  #depends indica que si se borra una venta, se borran sus items asociados 
-  has_many :items, dependent: :destroy
+  #depends indica que si se borra una venta, se borran sus items asociados, como el borrado es lógico, no pongo el destroy 
+  has_many :items
+
+
+ #Trigger para borrar logico de items al borrar logico de venta
+  after_update :return_stock, if: :saved_change_to_deleted?
+ #saved_change_to_deleted? es un metodo de Active Record que devuelve true si el atributo deleted ha cambiado en la ultima operacion de guardado.
+
 
   # Validations
   validates :datetime, presence: true
@@ -10,4 +16,15 @@ class Sale < ApplicationRecord
   # relaciones (a implementar)
   # belongs_to :employee
   # belongs_to :customer
+  private
+  def return_stock
+    if deleted
+      items.each do |item|
+        disk = item.disk
+        next unless disk && disk.has_attribute?(:stock)
+
+        disk.increment!(:stock, item.units_sold)
+      end
+    end
+  end
 end
