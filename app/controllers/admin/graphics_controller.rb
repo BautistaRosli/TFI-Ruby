1797ef9e2269896568d8f9e752f148dd1accccbe
MateoCarusotti <mateocarusotti@gmail.com>
2025-11-28 14:49:25 -0300

    Agrego prueba par ver si se muestran los graficos

--- app/controllers/admin/graphics_controller.rb
+++ app/controllers/admin/graphics_controller.rb
@@ -1,7 +1,9 @@
 class Admin::GraphicsController < ApplicationController
   def index
-    @inactive_properties = User.where(is_active: true).group_by_week(:created_at).count
-    @active_properties = User.where(is_active: false).group_by_week(:created_at).count
-    puts @inactive_properties
+    # is_active: false son las INACTIVAS
+    @inactive_properties = User.where(is_active: false).group_by_day(:created_at).count
+
+    # is_active: true son las ACTIVAS
+    @active_properties = User.where(is_active: true).group_by_day(:created_at).count
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  include ApexCharts::Helper
 end
