8f2fdcbfadca9cbffb5f9dafc75ea1cddf5dd7a2
MateoCarusotti <mateocarusotti@gmail.com>
2025-12-02 20:26:48 -0300

    Agrego manejador de excepcion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,11 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     previous_url = request.referer || root_path
-
-    flash[:alert] = "No tenés permisos para acceder a esa página."
-
+    if current_user # Si no estas logeado no te digo que rutas existen, para mas "seguridad".
+      flash[:alert] = "No tenés permisos para acceder a esa página."
+    else
+      flash[:alert] = "El recurso que estás buscando no existe."
+    end
     redirect_to previous_url
   end
 
@@ -26,6 +28,12 @@ class ApplicationController < ActionController::Base
     previous_url = request.referer || default_redirect_path
     redirect_to previous_url
   end
+
+    # agrego esto para manejar error con la session
+    rescue_from ActionController::InvalidAuthenticityToken do |exception|
+      flash[:alert] = "Hubo un problema con la sesión."
+      redirect_to new_user_session_path
+    end
 
   private
   def default_redirect_path
